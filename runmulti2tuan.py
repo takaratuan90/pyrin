e4b19ab38ba5105caa475329cb279bf385bec572
takaratuan90 <44602266+takaratuan90@users.noreply.github.com>
2023-12-07 08:23:10 +0700

    Update runmulti2tuan.py

--- runmulti2tuan.py
+++ runmulti2tuan.py
@@ -3,18 +3,16 @@ import multiprocessing  # Import module multiprocessing
 
 def command1():
     # Lệnh hoặc công việc bạn muốn chạy ở lệnh đầu tiên
-    subprocess.run(['python', '/content/pyrin/pyipad', '--utxoindex'])
+    subprocess.run(['/content/pyrin/pyipad', '--utxoindex'])
 
 def command2():
     # Lệnh hoặc công việc bạn muốn chạy ở lệnh thứ hai
-    
-    subprocess.run(['python', '/content/pyrin/pyrinminer', '--miningaddr', 'pyrin:qzsc50v3h0kg9fyhxeycafsumvtz237dhhucye9ucc8jjd0f3nvnzez6hj5vu'])
+    subprocess.run(['/content/pyrin/pyrinminer', '--miningaddr', 'pyrin:qzsc50v3h0kg9fyhxeycafsumvtz237dhhucye9ucc8jjd0f3nvnzez6hj5vu'])
 
 if __name__ == '__main__':
     # Tạo hai tiến trình riêng biệt cho mỗi lệnh
     process1 = multiprocessing.Process(target=command1)
     process2 = multiprocessing.Process(target=command2)
-
 
     # Bắt đầu chạy các tiến trình
     process1.start()
